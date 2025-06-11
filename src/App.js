7dc4b9d60a04b90ed25b1b24d4eff27b6e1a8d4d
devuser236 <skbaek87@naver.com>
2025-06-11 18:00:11 +0900

    0611#1

--- src/App.js
+++ src/App.js
@@ -2,80 +2,95 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Container, Nav, NavDropdown, Row, Col } from 'react-bootstrap';
 // import { BrowserView, MobileView, isBrowser, isMobile, isSafari, isChrome } from "react-device-detect";
+import {  Route, Routes, Link, href} from 'react-router-dom';
 import data from './data.js';
 import { useState } from 'react';
-
+import Detail from './routes/Detail.js';
 
 
 function App() {
-   const [goods] = useState(data);  
+   const [goods] = useState(data);
+   let [title, setTitle] = useState('aa');
+   const changeTitle = (value)=>{
+    setTitle(value)
+   }
   //  console.log(data);
    return (
     <div className="App">
-      <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">
-              <h4><strong>React-Shop</strong></h4>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">주문폭주⭐</Nav.Link>
-                <Nav.Link href="#link">6월 추천 아이템😎</Nav.Link>
-                <NavDropdown title="유아가구🛋️" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">테이블&체어</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">책장&정리함</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">침대</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="베이비👶🏻" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">베이비룸</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">아기체어</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">턱받이</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">유아용품</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <div className='main-bg'>
-        </div>
-        <div>
-          <br />
-          <br />
-          <br />
-          <h4 style={{textAlign : 'center', fontSize : '28px'}}><strong>베스트 상품🏆</strong></h4>
-          <br />
-          </div>
+      <Routes>
+        <Route path='/'element={
           <div>
-            
-            <Container>
-              <Row style={{textAlign : 'center'}} >
-                {
-                  goods.map(function(a, i){
-                    return(
-                      <GoodsList key={goods[i].id} goods={goods[i]} id={goods[i].id} />
-                    );
-                  })
-                }       
-              </Row>
-            </Container>
-        </div>
+            <Navbar expand="lg" className="bg-body-tertiary">
+              <Container>
+                <Navbar.Brand href="#home">
+                  <h4><strong>React-Shop</strong></h4>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">주문폭주⭐</Nav.Link>
+                    <Nav.Link href="#link">6월 추천 아이템😎</Nav.Link>
+                    <NavDropdown title="유아가구🛋️" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">테이블&체어</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">책장&정리함</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">침대</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="베이비👶🏻" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">베이비룸</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">아기체어</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">턱받이</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">유아용품</NavDropdown.Item>
+                    </NavDropdown>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+            <div className='main-bg'>
+            </div>
+            <div>
+              <br /><br /><br />
+              <h4 style={{textAlign : 'center', fontSize : '28px'}}><strong>베스트 상품🏆</strong></h4><br />
+            </div>
+            <div>
+              <Container>
+                <Row style={{textAlign : 'center'}} >
+                  {
+                    goods.map(function(a, i){
+                      return(
+                        <GoodsList key={goods[i].id} goods={goods[i]} id={goods[i].id} goodsTitle={goods[i].title}  changeTitle={changeTitle}/>
+                      );
+                    })
+                    }       
+                </Row>
+              </Container>
+            </div>
+        </div>}>
+        </Route>
+        <Route path='/detail' element={<Detail title={title}/>}/>
+      </Routes>
     </div>
   );
 }
 function GoodsList(props){
   return(
-    <Col xs>
-      <img src={process.env.PUBLIC_URL + '/images/goods'+props.id+'.png'} alt='goods' height='300px'/>
-      <p/>
-      <h4 style={{fontSize:'18px'}}><strong>{props.goods.title}</strong></h4>
-      {props.goods.content}<br/>
-      ₩{props.goods.price}
-      <br/>
-      <Button variant="secondary" style={{fontSize:'12px'}}>제품 상세보기</Button>
-      <br/>
-      <br/>
-    </Col>
+    <>
+      <Col xs>
+        <img src={process.env.PUBLIC_URL + '/images/goods'+props.id+'.png'} alt='goods' height='300px'/>
+        <p/>
+        <h4 style={{fontSize:'18px'}}><strong>{props.goods.title}</strong></h4>
+        {props.goods.content}<br/>
+        ₩{props.goods.price}        
+        <br/>
+          <Link to={'/detail'}><button variant="secondary" style={{fontSize:'12px'} } onClick={()=>{
+            let copyTitle=props.goodsTitle
+            props.changeTitle(copyTitle);
+          }}>제품상세보기</button></Link>
+        <br/>
+        <br/>
+      </Col>
+    </>
   );
 }
+
+
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,10 @@
+import App from '../App';
+function Detail(props){
+  return(
+    <div>
+      <p>{props.title}  상세 페이지</p>
+    </div>
+  )
+}
+
+export default Detail
